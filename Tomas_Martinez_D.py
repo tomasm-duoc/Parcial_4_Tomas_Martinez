94ec1536aad707257508beaf9abccb683e0977af
Tomás Martínez <to.martinezg@duocuc.cl>
2025-06-30 09:50:15 -0400

    Opcion 1 de menu añadida

--- Tomas_Martinez_D.py
+++ Tomas_Martinez_D.py
@@ -1,6 +1,25 @@
+totem_autoservicio = {
+    "compradores":
+    [
+        {
+            "nombre_comprador": "nombre1",      
+        },
+        {
+            "nombre_comprador": "Nombre2",    # Nombres de comprador "placeholder" para testeo
+        }
+    ]
+}
 
+stock_entradas_conce = 500
+stock_entradas_puente = 1300
+stock_entradas_valpo = 100
+stock_entradas_vinia = 50
+
+codigo_confirmacion = "Tengoelpoderr42" # Codigo necesario para opción 1 - Compra en Concepción
 
 def verificacion_entero(mensaje:str):
+    """Esta funcíon verifica que el input del usuario entregue solamente números.
+       La variable "mensaje" sirve para modificar el mensaje que dara el input"""
     while True:
         try:
             numero = int(input(mensaje))
@@ -8,6 +27,22 @@ def verificacion_entero(mensaje:str):
             print("\nERROR - Ingrese un valor númerico.")
         else:
             return numero
+
+def verificacion_comprador(nombre:str):
+    """Esta función recorre la lista compradores y verifica que el elemento entregado
+    (en este caso el nombre de un comprador), no este repetido. En el caso de que se repita entregara False"""
+    for i in totem_autoservicio["compradores"]:
+        nombre_lista = i["nombre_comprador"]
+        if nombre.lower() == nombre_lista.lower():
+            return False
+        
+def verificacion_string(mensaje:str):
+    """Esta función verifica que el input del usuario no sea un string vacio"""
+    string = input(mensaje)
+    if len(string.strip()) < 1:
+        print("\nERROR - No puede entregar un texto vacio.")
+    else:
+        return string
 
 
 while True:
@@ -20,19 +55,37 @@ while True:
 
     opcion = verificacion_entero("\nIngrese su opción: ")
 
-    if opcion == 1:
+    if opcion == 1: # Concepción
+        if stock_entradas_conce < 1:
+            print("No hay entradas disponibles!")
+            continue
+        print("- Compra en Concepción -")
+        nombre_comprador = verificacion_string("Nombre del comprador: ")
+
+        if verificacion_comprador(nombre_comprador) == False:
+            print("\nERROR - Ese nombre de comprador ya existe!")
+        else:
+            confirmar_codigo = verificacion_string("Código de confirmación: ")
+
+            if confirmar_codigo == codigo_confirmacion:
+                comprador_añadir = {"nombre_comprador": nombre_comprador} # Convertir nombre en diccionario
+                totem_autoservicio["compradores"].append(comprador_añadir) # Añadir comprador a la lista
+                stock_entradas_conce =- 1
+                print(f"Entrada registrada! Stock restante: {stock_entradas_conce}")
+            else:
+                print("\nERROR - Código de acceso invalido")
+
+    
+    elif opcion == 2: # Puente Alto
         print(f"opcion = {opcion}")
     
-    elif opcion == 2:
+    elif opcion == 3: # Muelle Barón, Valparaíso
         print(f"opcion = {opcion}")
     
-    elif opcion == 3:
+    elif opcion == 4: # Muelle Vergara, Viña del Mar
         print(f"opcion = {opcion}")
     
-    elif opcion == 4:
-        print(f"opcion = {opcion}")
-    
-    elif opcion == 5:
+    elif opcion == 5: # Salir
         break
 
     else:
